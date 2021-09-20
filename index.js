d707674fc0b6e509fca474556a33b2cce3181ebb
Justin Chittarath <justinchitt@gmail.com>
2021-09-20 15:34:49 -0600

    commit tree f7a3368b5a8b9948d858dc32729a95af8ff977cd

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (2500 < feet) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+   let checkCity = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return checkCity;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
